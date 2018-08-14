20570abc2c1e6b226f3e24e534781fb63332afb1
tharanamayu <tharana.maya@gmail.com>
2018-08-14 23:07:40 +0530

    creating project's mainui

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindowUI.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("EmployeeUI.fxml"));
         
         stageBasic = stage;
         
