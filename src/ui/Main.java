bc7e73f19efdb52b1b3bc5f26cf718d6b48c999a
tharanamayu <tharana.maya@gmail.com>
2018-08-14 23:13:06 +0530

    chainging main

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("EmployeeUI.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindowController.fxml"));
         
         stageBasic = stage;
         
