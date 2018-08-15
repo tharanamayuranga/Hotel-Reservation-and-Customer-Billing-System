29f66283fd5bfd26fa92378b6dd004fda2e0106f
tharanamayu <tharana.maya@gmail.com>
2018-08-15 22:25:42 +0530

    adding css file

---
+++ src/ui/Style.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ui;
+
+/**
+ *
+ * @author Suranga
+ */
+public class Style {
+    
+     public static String valid = "-fx-background-color:#20edc7;";
+     public static String invalid = "-fx-background-color:pink;";
+     public static String updated = "-fx-background-color:#f5ff90";
+     public static String initial = "-fx-background-color:#9fd4e4"; 
+    
+}
