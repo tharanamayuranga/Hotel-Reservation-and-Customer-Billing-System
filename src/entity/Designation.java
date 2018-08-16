b2ad6304141885abf517540e002ac5bec2a26bdb
tharanamayu <tharana.maya@gmail.com>
2018-08-16 23:23:58 +0530

    created designation entity class from db

---
+++ src/entity/Designation.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Tharana
+ */
+@Entity
+@Table(name = "designation")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Designation.findAll", query = "SELECT d FROM Designation d")
+    , @NamedQuery(name = "Designation.findById", query = "SELECT d FROM Designation d WHERE d.id = :id")
+    , @NamedQuery(name = "Designation.findByName", query = "SELECT d FROM Designation d WHERE d.name = :name"),
+
+    //Binding 
+    @NamedQuery(name = "Designation.findAllByName", query = "SELECT d FROM Designation d WHERE d.name = :name"),
+
+})
+public class Designation implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "designationId", fetch = FetchType.LAZY)
+    private List<Employee> employeeList;
+
+    public Designation() {
+    }
+
+    public Designation(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean setName(String name) {
+        
+         boolean validity = name != null && name.matches("[A-Z][_A-Za-z]{1}[_A-Za-z]*([_A-Za-z]*\\s[_A-Za-z]*)*"); 
+        
+        if(validity){
+            
+            this.name = name; 
+            
+        } else { 
+            
+            this.name=null;
+        
+        } 
+        
+        return validity;
+        
+    }
+
+    @XmlTransient
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Designation)) {
+            return false;
+        }
+        Designation other = (Designation) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+}
