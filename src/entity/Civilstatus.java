371723c34d45e11283e390a1078e4e295885801c
tharanamayu <tharana.maya@gmail.com>
2018-08-16 23:24:52 +0530

    created civilstatus entity class from db

---
+++ src/entity/Civilstatus.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Tharana
+ */
+@Entity
+@Table(name = "civilstatus")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Civilstatus.findAll", query = "SELECT c FROM Civilstatus c")
+    , @NamedQuery(name = "Civilstatus.findById", query = "SELECT c FROM Civilstatus c WHERE c.id = :id")
+    , @NamedQuery(name = "Civilstatus.findByName", query = "SELECT c FROM Civilstatus c WHERE c.name = :name")})
+public class Civilstatus implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "civilstatusId", fetch = FetchType.LAZY)
+    private List<Employee> employeeList;
+
+    public Civilstatus() {
+    }
+
+    public Civilstatus(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @XmlTransient
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Civilstatus)) {
+            return false;
+        }
+        Civilstatus other = (Civilstatus) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+}
