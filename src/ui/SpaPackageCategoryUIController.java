c810859235fe0c60bc546281388beaf6bbf29cd1
tharanamayu <tharana.maya@gmail.com>
2018-09-14 23:32:15 +0530

    updated SpaPackageCategoryUIController and UI

--- src/ui/SpaPackageCategoryUIController.java
+++ src/ui/SpaPackageCategoryUIController.java
@@ -56,6 +56,96 @@ public class SpaPackageCategoryUIController implements Initializable {
 
 //</editor-fold>
     
+    //<editor-fold defaultstate="collapsed" desc="Initializing-Mthods">
     
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        initial = Style.initial;
+        valid = Style.valid;
+        invalid = Style.invalid;
+        updated = Style.updated;
+
+        loadForm();
+
+        loadTable();
+    }
+
+    private void loadForm() {
+
+        spaCategory = new Spapackagecategory();
+        oldSpaCategory = null;
+
+        txtCategoryType.setText("");
+
+        dissableButtons(false, false, true, true);
+
+        setStyle(initial);
+
+    }
+
+    private void setStyle(String style) {
+
+        txtCategoryType.setStyle(style);
+
+    }
+
+    private void dissableButtons(boolean select, boolean insert, boolean update, boolean delete) {
+
+        btnAdd.setDisable(insert);
+        btnUpdate.setDisable(update);
+        btnDelete.setDisable(delete);
+
+    }
+
+    private void loadTable() {
+
+        lstCategoryType.setItems(SpaPackageCategoryDao.getAll());
+        lstCategoryType.refresh();
+
+    }
+//</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Binding-Methods">
+    @FXML
+    private void txtCustomerIDTypeKR(KeyEvent event) {
+        List dename = SpaPackageCategoryDao.getAllByName(txtCategoryType.getText().trim());
+
+        if (spaCategory.setName(txtCategoryType.getText().trim())) {
+
+            if (dename.isEmpty()) {
+
+                if (oldSpaCategory != null && !spaCategory.getName().equals(oldSpaCategory.getName())) {
+
+                    txtCategoryType.setStyle(updated);
+
+                } else {
+
+                    txtCategoryType.setStyle(valid);
+
+                }
+
+            } else {
+
+                if (oldSpaCategory != null && oldSpaCategory.getName().equals(spaCategory.getName())) {
+
+                    txtCategoryType.setStyle(valid);
+
+                } else {
+
+                    txtCategoryType.setStyle(invalid);
+                    spaCategory.setName(null);
+
+                }
+
+            }
+
+        } else {
+
+            txtCategoryType.setStyle(invalid);
+
+        }
+    }
+//</editor-fold>
+
     
 }
