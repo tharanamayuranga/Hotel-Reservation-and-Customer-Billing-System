3bae9a2bb27f878adbfaf8c4019f71af060ca138
tharanamayu <tharana.maya@gmail.com>
2018-09-16 09:48:34 +0530

    updated PrivilageUIController

--- src/ui/PrivilageController.java
+++ src/ui/PrivilageController.java
@@ -413,6 +413,305 @@ public class PrivilageController implements Initializable {
     }
 //</editor-fold>
 
+    //<editor-fold defaultstate="collapsed" desc="Operation Methods">
+    @FXML
+    private void btnUpdateAP(ActionEvent event) {
+         String errors = getErrors();
+        
+        if ( errors.isEmpty() ) {
 
+            String updates = getUpdates();
+
+            if (!updates.isEmpty()) {
+
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+
+                alert.setTitle("Update Module");
+                alert.setHeaderText("Are you sure you need to update the following Module");
+                alert.setContentText(updates);
+                
+                DialogPane dialogPane = alert.getDialogPane();
+
+                dialogPane.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+                dialogPane.getStyleClass().add("myDialogForConfirmation");
+
+                Optional<ButtonType> result = alert.showAndWait();
+
+                if (result.get() == ButtonType.OK) {
+                    
+                    //Notification.Notifier.INSTANCE.notifySuccess("Update", privilegeForForm.getModuleId().getName() + " is updated!");
+
+                    PrivilegeDao.update(privilegeForForm);
+
+                    loadForm();
+                    loadTable();
+
+                }
+
+            } else {
+                
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+                alert.setTitle("Update Module");
+                alert.setHeaderText("There is nothing to Update!!!");
+                alert.setContentText("Nothing to Update!!!");
+                
+                DialogPane dialogPane = alert.getDialogPane();
+
+                dialogPane.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+                dialogPane.getStyleClass().add("myDialogForInformation");
+                
+                alert.showAndWait();
+                
+            }
+            
+        } else {
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+
+            alert.setTitle("Error - Employee Update");
+            alert.setHeaderText("Form Data Error");
+            alert.setContentText(errors);
+            
+            DialogPane dialogPane = alert.getDialogPane();
+
+            dialogPane.getStylesheets().add(getClass().getResource("/css/style1.css").toExternalForm());
+            dialogPane.getStyleClass().add("myDialogForError");
+            
+            alert.showAndWait();
+
+        }
+    }
+    
+
+    @FXML
+    private void btnClearAP(ActionEvent event) {
+         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+
+            alert.setTitle(" Privilege Management");
+            alert.setHeaderText("Clear Form");
+            alert.setContentText("Are you sure you need to clear form ???");
+            
+            DialogPane dialogPane = alert.getDialogPane();
+
+            dialogPane.getStylesheets().add(getClass().getResource("/css/style1.css").toExternalForm());
+            dialogPane.getStyleClass().add("myDialogForConfirmation");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            
+            if (result.get() == ButtonType.OK) {
+                
+                //Notification.Notifier.INSTANCE.notifySuccess("Clear Form", "The Form is cleared!" );
+                
+                loadForm();     
+                
+            }
+    }
+
+    @FXML
+    private void btnAddAP(ActionEvent event) {
+        privilegeForForm.setSel(cbxSelect.isSelected()?1:0); // validation and binding 
+        privilegeForForm.setIns(cbxInsert.isSelected()?1:0);
+        privilegeForForm.setUpd(cbxUpdate.isSelected()?1:0);
+        privilegeForForm.setDel(cbxDelete.isSelected()?1:0);
+        
+        String errors = getErrors();       
+        
+        if ( errors.isEmpty() ) {
+            
+             String details = "Are you sure you need to add this Privileges with following details\n "
+                    + "\nRole           \t:" + privilegeForForm.getRoleId().getName()
+                    + "\nModule         \t:" + privilegeForForm.getModuleId().getName()
+                    + "\nSelect         \t:" + ( privilegeForForm.getSel() == 0 ? "Not Selected" : "Selected") 
+                    + "\nInsert         \t:" + ( privilegeForForm.getIns() == 0 ? "Not Selected" : "Selected")
+                    + "\nUpdate         \t:" + ( privilegeForForm.getUpd() == 0 ? "Not Selected" : "Selected")
+                    + "\nDelete         \t:" + ( privilegeForForm.getDel() == 0 ? "Not Selected" : "Selected");
+                   
+            
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Add Privilege");
+            alert.setHeaderText("Are you sure you need to add the following Privilege");
+            alert.setContentText(details);
+            
+            DialogPane dialogPane = alert.getDialogPane();
+
+            dialogPane.getStylesheets().add(getClass().getResource("/css/style1.css").toExternalForm());
+            dialogPane.getStyleClass().add("myDialogForConfirmation");
+            
+            Optional<ButtonType> result = alert.showAndWait();
+            
+            if (result.get() == ButtonType.OK) {
+                
+               // Notification.Notifier.INSTANCE.notifySuccess("Save", privilegeForForm.getModuleId().getName() + " is saved!");
+                
+                PrivilegeDao.add(privilegeForForm);
+
+                loadTable();
+                loadForm();
+            
+            }   
+            
+        }else{
+            
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("You need to fill the following Privilege");
+            alert.setContentText(errors);
+            
+            DialogPane dialogPane = alert.getDialogPane();
+
+            dialogPane.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+            dialogPane.getStyleClass().add("myDialogForError");
+            
+            alert.showAndWait();
+        
+        }
+    }
+
+    @FXML
+    private void btnDeleteAP(ActionEvent event) {
+        String details =
+                  "\nRole                   \t: " + oldPrivilegeForForm.getRoleId().getName()
+                + "\nModule Type    \t: " + oldPrivilegeForForm.getModuleId().getName()
+                + "\nSelect                 \t: " + (oldPrivilegeForForm.getSel() == 0 ? "Not Selected" : "Selected")
+                + "\nInsert                 \t: " + (oldPrivilegeForForm.getIns() == 0 ? "Not Selected" : "Selected")
+                + "\nUpdate             \t: " + (oldPrivilegeForForm.getUpd() == 0 ? "Not Selected" : "Selected")
+                + "\nDelete                 \t: " + (oldPrivilegeForForm.getDel() == 0 ? "Not Selected" : "Selected");
+
+            
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Delete Privilege");
+        alert.setHeaderText("Are you sure you need to delete the following Privilege?");
+        alert.setContentText(details);
+        
+//        DialogPane dialogPane = alert.getDialogPane();
+//
+//        dialogPane.getStylesheets().add(getClass().getResource("/css/style1.css").toExternalForm());
+//        dialogPane.getStyleClass().add("myDialogForConfirmation");
+
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.get() == ButtonType.OK) {
+            
+           // Notification.Notifier.INSTANCE.notifySuccess("Delete",  oldPrivilegeForForm.getModuleId().getName() + " is deleted!" );
+
+            PrivilegeDao.delete(oldPrivilegeForForm);
+
+//            loadTable();
+            fillTable(PrivilegeDao.getAllPrivilege()); 
+            loadForm();
+            
+
+            pagination.setCurrentPageIndex(page);
+            tblPrivilege.getSelectionModel().select(row);
+            
+        }      
+    }
+
+    private String getErrors(){
+    
+        String errors = "";
+        
+         if (privilegeForForm.getRoleId() == null) {
+            errors = errors + "Role \t\tis Not Selected\n";
+        }
+        if (privilegeForForm.getModuleId() == null) {
+            errors = errors + "Module \t\tis Not Selected\n";
+        }
+//        if (!(cbxSelect.isSelected()|| cbxInsert.isSelected()|| cbxUpdate.isSelected()|| cbxDelete.isSelected())) {
+//            errors = errors + "Privilege \t\tis Not Selected\n";
+//        }
+        
+        return errors;
+    
+    }
+     private String getUpdates(){
+        
+        String updates = "";
+        
+         if ( oldPrivilegeForForm != null ) {
+             
+             privilegeForForm.setSel(cbxSelect.isSelected() ? 1 : 0);             
+             privilegeForForm.setIns(cbxInsert.isSelected() ? 1 : 0);
+             privilegeForForm.setUpd(cbxUpdate.isSelected() ? 1 : 0);
+             privilegeForForm.setDel(cbxDelete.isSelected() ? 1 : 0);
+             
+             if (!privilegeForForm.getSel().equals(oldPrivilegeForForm.getSel())) {
+                 updates = updates + "Select Changed " + ( oldPrivilegeForForm.getSel() == 1 ? "Selecteed" : "Not Selected" ) + " to " + ( privilegeForForm.getSel()  == 1 ? "Selecteed" : "Not Selected") + "\n";
+             }
+             
+             if (!privilegeForForm.getIns().equals(oldPrivilegeForForm.getIns())) {
+                 updates = updates + "Insert Changed " + ( oldPrivilegeForForm.getIns()  == 1 ? "Selecteed" : "Not Selected" ) + " to " + ( privilegeForForm.getIns()  == 1 ? "Selecteed" : "Not Selected") + "\n";
+             }
+             
+             if (!privilegeForForm.getUpd().equals(oldPrivilegeForForm.getUpd())) {
+                 updates = updates + "Update Changed " + ( oldPrivilegeForForm.getUpd()  == 1 ? "Selecteed" : "Not Selected") + " to " + ( privilegeForForm.getUpd()  == 1 ? "Selecteed" : "Not Selected") + "\n";
+             }
+             if (!privilegeForForm.getDel().equals(oldPrivilegeForForm.getDel())) {
+                 updates = updates + "Delete Changed " + ( oldPrivilegeForForm.getDel()  == 1 ? "Selecteed" : "Not Selected") + " to " + ( privilegeForForm.getDel()  == 1 ? "Selecteed" : "Not Selected") + "\n";
+             }
+             
+         }
+
+        return updates;
+     
+     }
+      @FXML
+    private void tblPrivilegeMC(MouseEvent event) {
+        fillForm();
+    }
+
+    @FXML
+    private void tblPrivilegeKR(KeyEvent event) {
+        fillForm();
+    }
+     private void fillForm() {
+          
+         if ( tblPrivilege.getSelectionModel().getSelectedItem() != null ) {
+             
+//             cmbModule.setEditable(false);
+             
+             dissableButtons(true, true, false, false);
+             
+             setStyle(valid);
+             
+             cmbSearchModule.setStyle(initial);
+             cmbSearchRole.setStyle(initial);
+             
+             oldPrivilegeForForm = PrivilegeDao.getById(tblPrivilege.getSelectionModel().getSelectedItem().getId());
+             privilegeForForm = PrivilegeDao.getById(tblPrivilege.getSelectionModel().getSelectedItem().getId());
+
+//         System.out.println(oldPrivilegeForForm.getModuleId());
+//         System.out.println(privilegeForForm.getModuleId());
+
+             cmbRole.getItems().clear();
+             cmbRole.setItems(FXCollections.observableArrayList((Role) oldPrivilegeForForm.getRoleId()));             
+             cmbRole.getSelectionModel().select(0);
+             
+             cmbModule.getItems().clear();
+             cmbModule.setItems(FXCollections.observableArrayList((Module) oldPrivilegeForForm.getModuleId()));             
+             cmbModule.getSelectionModel().select(0);
+             
+             cbxSelect.setSelected(oldPrivilegeForForm.getSel() == 1);
+             cbxInsert.setSelected(oldPrivilegeForForm.getIns() == 1);
+             cbxUpdate.setSelected(oldPrivilegeForForm.getUpd() == 1);
+             cbxDelete.setSelected(oldPrivilegeForForm.getDel() == 1);
+
+       //     PrivilegeDao.getById(); 
+       
+            page = pagination.getCurrentPageIndex();                   
+            row = tblPrivilege.getSelectionModel().getSelectedIndex();
+            
+           // Notification.Notifier.INSTANCE.notifyInfo("Selected", oldPrivilegeForForm.getModuleId().getName() + " is selected!");
+             
+        }
+         
+    }
+    @FXML
+    private void lblAddNewRoleMC(MouseEvent event) {
+    }
+//</editor-fold>
+    
+ 
    
 }
