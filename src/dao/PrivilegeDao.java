b0a57aa6f0d835326e930d8291dd88fb901f5dd8
tharanamayu <tharana.maya@gmail.com>
2018-09-16 09:56:00 +0530

    created PrivilegeDao

---
+++ src/dao/PrivilegeDao.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import entity.Module;
+import entity.Privilege;
+import entity.Role;
+import java.util.HashMap;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author Tharana
+ */
+public class PrivilegeDao {
+    
+    public static ObservableList<Privilege> getAll() {     
+        
+        return CommonDao.select("Privilege.findAll");
+        
+    }
+    
+
+}
