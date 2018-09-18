33b1260d27b22a9737a2d8e2f11d3b244cd00d84
tharanamayu <tharana.maya@gmail.com>
2018-09-18 23:13:20 +0530

    created Floor entity

---
+++ src/entity/Floor.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Tharana
+ */
+@Entity
+@Table(name = "floor")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Floor.findAll", query = "SELECT f FROM Floor f")
+    , @NamedQuery(name = "Floor.findById", query = "SELECT f FROM Floor f WHERE f.id = :id")
+    , @NamedQuery(name = "Floor.findByNo", query = "SELECT f FROM Floor f WHERE f.no = :no")})
+public class Floor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "no")
+    private String no;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "floorId", fetch = FetchType.EAGER)
+    private List<Room> roomList;
+
+    public Floor() {
+    }
+
+    public Floor(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    @XmlTransient
+    public List<Room> getRoomList() {
+        return roomList;
+    }
+
+    public void setRoomList(List<Room> roomList) {
+        this.roomList = roomList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Floor)) {
+            return false;
+        }
+        Floor other = (Floor) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return no;
+    }
+    
+}
