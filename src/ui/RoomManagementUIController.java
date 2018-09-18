1c8c1377c9c78b261b11f5dc82a098b74740cdbb
tharanamayu <tharana.maya@gmail.com>
2018-09-18 23:11:44 +0530

    updated RoomManagementUIController

--- src/ui/RoomManagementUIController.java
+++ src/ui/RoomManagementUIController.java
@@ -297,6 +297,288 @@ public class RoomManagementUIController implements Initializable {
 
 //</editor-fold>
     
+    //<editor-fold defaultstate="collapsed" desc="Operation Methods">
+    @FXML
+    private void btnAddAP(ActionEvent event) {
+        String errors = getErrors();
+
+        if (errors.isEmpty()) {
+
+            String details
+                    = "\nRoom No          \t\t: " + room.getNo()
+                    + "\nRoom Type  \t\t: " + room.getRoomtypeId()
+                    + "\nFloor No  \t\t: " + room.getFloorId()
+                    + "\nRoom Status        \t\t: " + room.getRoomstatusId().getName();
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Add Module");
+            alert.setHeaderText("Are you sure you need to add the following Food Item???");
+            alert.setContentText(details);
+
+            DialogPane dialogPane = alert.getDialogPane();
+
+            dialogPane.getStylesheets().add(getClass().getResource("/css/style1.css").toExternalForm());
+            dialogPane.getStyleClass().add("myDialogForConfirmation");
+
+            Optional<ButtonType> result = alert.showAndWait();
+
+            if (result.get() == ButtonType.OK) {
+
+                // employee.setDisable(0);
+                RoomDao.add(room);
+
+                loadForm();
+
+                loadTable();
+
+            }
+
+        } else {
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("You need to fill the following Employee");
+            alert.setContentText(errors);
+
+            DialogPane dialogPane = alert.getDialogPane();
+
+            dialogPane.getStylesheets().add(getClass().getResource("/css/style1.css").toExternalForm());
+            dialogPane.getStyleClass().add("myDialogForError");
+
+            alert.showAndWait();
+
+        }
+    }
+
+    @FXML
+    private void btnDeleteAP(ActionEvent event) {
+         
+                     String details
+                    = "\nRoom No          \t\t: " + room.getNo()
+                    + "\nRoom Type  \t\t: " + room.getRoomtypeId()
+                    + "\nFloor No  \t\t: " + room.getFloorId()
+                    + "\nRoom Status        \t\t: " + room.getRoomstatusId().getName();
+                     
+        Alert alertForDelete = new Alert(Alert.AlertType.CONFIRMATION);
+
+        alertForDelete.setTitle("Delete Module");
+        alertForDelete.setHeaderText("Are you sure you need to delete the following Module?");
+        alertForDelete.setContentText(details);
+
+        DialogPane dialogPane = alertForDelete.getDialogPane();
+
+        dialogPane.getStylesheets().add(getClass().getResource("/css/style1.css").toExternalForm());
+        dialogPane.getStyleClass().add("myDialogForConfirmation");
+
+        Optional<ButtonType> result = alertForDelete.showAndWait();
+
+        if (result.get() == ButtonType.OK) {
+
+//            EmployeeDao.delete(oldEmployee);
+//oldFoodItem.setDisable(1);
+            RoomDao.delete(room);
+
+//            fillTable(EmployeeDao.getAll());
+            loadTable();
+
+            loadForm();
+
+            pagination.setCurrentPageIndex(page);
+            tblRoom.getSelectionModel().select(row);
+
+        }
+    }
+
+    @FXML
+    private void btnClearAP(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+
+        DialogPane dialogPane = alert.getDialogPane();
+
+        dialogPane.getStylesheets().add(getClass().getResource("/css/style1.css").toExternalForm());
+        dialogPane.getStyleClass().add("myDialogForConfirmation");
+
+        alert.setTitle(" Room Management Management");
+        alert.setHeaderText("Clear Form");
+        alert.setContentText("Are you sure you need to clear form?? ");
+
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.get() == ButtonType.OK) {
+
+            loadForm();
+
+        }
+    }
+
+    @FXML
+    private void btnUpdateAP(ActionEvent event) {
+         String errors = getErrors();
+
+        if (errors.isEmpty()) {
+
+//            DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+//
+//            try {
+//
+//                employee.setDob(java.sql.Date.valueOf(format.format(format.parse(actualDob))));
+//
+//            } catch (ParseException ex) {
+//
+//                System.out.println(ex);
+//
+//            }
+            String updates = getUpdates();
+
+            if (!updates.isEmpty()) {
+
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+
+                alert.setTitle("Update Module");
+                alert.setHeaderText("Are you sure you need to update the following Module");
+                alert.setContentText(updates);
+
+                DialogPane dialogPane = alert.getDialogPane();
+
+                dialogPane.getStylesheets().add(getClass().getResource("/css/style1.css").toExternalForm());
+                dialogPane.getStyleClass().add("myDialogForConfirmation");
+
+                Optional<ButtonType> result = alert.showAndWait();
+
+                if (result.get() == ButtonType.OK) {
+
+                    //  Notification.Notifier.INSTANCE.notifySuccess("Update", employee.getName() + " is updated!");
+                    RoomDao.update(room);
+
+                    loadForm();
+                    loadTable();
+
+                }
+
+            } else {
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+                alert.setTitle("Update Module");
+                alert.setHeaderText("There is nothing to Update!!!");
+                alert.setContentText("Nothing to Update!!!");
+
+                DialogPane dialogPane = alert.getDialogPane();
+
+                dialogPane.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+                dialogPane.getStyleClass().add("myDialogForInformation");
+
+                alert.showAndWait();
+
+            }
+
+        } else {
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+
+            alert.setTitle("Error - Employee Update");
+            alert.setHeaderText("Form Data Error");
+            alert.setContentText(errors);
+
+            DialogPane dialogPane = alert.getDialogPane();
+
+            dialogPane.getStylesheets().add(getClass().getResource("/css/style1.css").toExternalForm());
+            dialogPane.getStyleClass().add("myDialogForError");
+
+            alert.showAndWait();
+
+        }
+    }
+
+    @FXML
+    private void tblRoomMC(MouseEvent event) {
+        fillForm();
+    }
+
+    @FXML
+    private void tblRoomKR(KeyEvent event) {
+        fillForm();
+    }
+
+    private String getErrors() {
+
+        String errors = "";
+
+        if (room.getNo() == null) {
+            errors = errors + "Room No \t\tis Invalid\n";
+        }
+
+        if (room.getFloorId() == null) {
+            errors = errors + "Floor No \t\tis Not Selected\n";
+        }
+        if (room.getRoomtypeId() == null) {
+            errors = errors + "Room Type \t\tis Not Selected\n";
+        }
+        if (room.getRoomstatusId() == null) {
+            errors = errors + "Room Status \t\tis Not Selected\n";
+        }
+
+        return errors;
+
+    }
+
+    private String getUpdates() {
+
+        String updates = "";
+
+        if (oldRoom != null) {
+
+            if (room.getNo() != null && !room.getNo().equals(oldRoom.getNo())) {
+                updates = updates + oldRoom.getNo() + " chnaged to " + room.getNo() + "\n";
+            }
+            if (room.getFloorId() != null && !room.getFloorId().equals(oldRoom.getFloorId())) {
+                updates = updates + oldRoom.getFloorId() + " chnaged to " + room.getFloorId() + "\n";
+            }
+            if (room.getRoomtypeId() != null && !room.getRoomtypeId().equals(oldRoom.getRoomtypeId())) {
+                updates = updates + oldRoom.getRoomtypeId() + " chnaged to " + room.getRoomtypeId() + "\n";
+            }
+            if (room.getRoomstatusId() != null && !room.getRoomstatusId().equals(oldRoom.getRoomstatusId())) {
+                updates = updates + oldRoom.getRoomstatusId() + " chnaged to " + room.getRoomstatusId() + "\n";
+            }
+
+        }
+
+        return updates;
+
+    }
+
+    private void fillForm() {
+
+        if (tblRoom.getSelectionModel().getSelectedItem() != null) {
+
+            dissableButtons(false, true, false, false);
+            setStyle(valid);
+
+            oldRoom = RoomDao.getById(tblRoom.getSelectionModel().getSelectedItem().getId());
+            room = RoomDao.getById(tblRoom.getSelectionModel().getSelectedItem().getId());
+
+//            cmbSearchDesignation.setStyle(initial);
+//            cmbSearchStatus.setStyle(initial);
+//            txtSearchName.setStyle(initial);
+            txtRoomNo.setText(oldRoom.getNo());
+            cmbFloorNo.getSelectionModel().select((Floor) room.getFloorId());
+            cmbRoomType.getSelectionModel().select((Roomtype) room.getRoomtypeId());
+            cmbRoomStatus.getSelectionModel().select((Roomstatus) room.getRoomstatusId());
+
+            
+
+            page = pagination.getCurrentPageIndex();
+            row = tblRoom.getSelectionModel().getSelectedIndex();
+
+//            oldNicWithOldFormateWithV = oldEmployee.getNic().substring(2, 7) + oldEmployee.getNic().substring(8) + "v";
+//            oldNicWithOldFormateWithX = oldEmployee.getNic().substring(2, 7) + oldEmployee.getNic().substring(8) + "x";
+//            lblDob.setText(oldEmployee.getDob().toString());
+//            lblGender.setText(oldEmployee.getGenderId().getName());
+        }
+
+    }
+//</editor-fold>
+
 
 
     
